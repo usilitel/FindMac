0f6c8da70ea42ee36ac1b49ed07d21690076ec4e
usilitel <usilitel00@gmail.com>
2019-08-08 15:28:33 +0300

    interface added

--- app/src/main/java/com/usilitel/findmac/Client.kt
+++ app/src/main/java/com/usilitel/findmac/Client.kt
@@ -1,16 +1,24 @@
 package com.usilitel.findmac
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.RemoteMessage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.aspectj.bridge.Version.text
 
 object Client {
 
     private val server = Server
     private val utils = NotificationUtils
+
+    private var _macAddressStatus = MutableLiveData<String>().apply { value = "" }
+    val macAddressStatus : LiveData<String>
+        get() = _macAddressStatus
+
 
     // отправляем с клиента на сервер запрос с mac-адресом
     public  fun sendRequest(macAddress: String){
@@ -18,6 +26,7 @@ object Client {
         dataParams.put("macAddress", macAddress)
         dataParams.put("clientRegistrationToken", FirebaseInstanceId.getInstance().token!!)
 
+        _macAddressStatus.postValue("request sent...")
         GlobalScope.launch ( Dispatchers.IO ){
             NotificationUtils.sendNotification(utils.request_title, server.serverRegistrationToken!!, dataParams)
         }
@@ -31,6 +40,8 @@ object Client {
 
             Log.d("PushNotificationReceive", "Server response data: " + remoteMessage.data)
             Log.d("PushNotificationReceive", "Server response: macAddress " + dataMacAddress + " is active = " + dataMacAddressIsActive)
+
+            _macAddressStatus.postValue(dataMacAddress + " is active = " + dataMacAddressIsActive)
         }
     }
 
--- app/src/main/java/com/usilitel/findmac/MainActivity.kt
+++ app/src/main/java/com/usilitel/findmac/MainActivity.kt
@@ -2,6 +2,8 @@ package com.usilitel.findmac
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // проверяем есть ли mac-адрес в сети на сервере
-        client.sendRequest("8c:64:22:ad:e2:04")
+        client.macAddressStatus.observe(this@MainActivity, Observer {
+            if(it==null) return@Observer
+            textViewStatus.text = it
+        })
 
+        editTextMacAddress.text.clear()
+        editTextMacAddress.text.insert(0,"8c:64:22:ad:e2:04")
+
+        buttonSendRequest.setOnClickListener { sendRequest() }
     }
+
+    fun sendRequest(){
+        // проверяем есть ли mac-адрес в сети на сервере
+        client.sendRequest(editTextMacAddress.text.toString())
+    }
+
 
 }
 
